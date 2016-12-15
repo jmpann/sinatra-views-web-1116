6b529ab60d313b437ee827a04a4c04b7d7904305
Josh Pann <josh.pann@example.com>
2016-12-15 13:24:13 -0500

    done

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
+		#this is a Sinatra block responding to the GET method, matching / to user input, executing do block
+		#syntax erb (method) :page
+		#sinatra render erb filve "index" in views dir
+	end
+
+	get '/info' do
+		erb :info
 	end
 end
